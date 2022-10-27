2604fe40630c5ff7c24fdd9ae2edc4f13a42dfe4
Hiraya8823 <hirayama82007@yahoo.co.jp>
2022-10-27 09:02:14 +0900

    #5

---
+++ server/product_cart.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<?php include_once __DIR__ . '/head.html' ?> 
+<body>
+    <?php include_once __DIR__ . '/_header.html' ?>
+    <div id="product_carts" class="product_carts">
+        <div class="box">
+            <h1 class="left wrapper">
+                Your Cart
+            </h1>
+            <div class="right">
+                <div class="product_cart">
+                    <div class="product_cart_img">
+                        <img src="../images/古着1.webp" alt="">
+                    </div>
+                    <div class="product_cart_detail">
+                        <div class="product_name">
+                            <p>Reebok/"LONDON WEWBLEY STADIUM 28.OCT.07"Foodie</p>
+                        </div>
+                        <h2 class="product_price">
+                            3,750JPY
+                        </h2>
+                    </div>
+                </div>
+                <div class="product_cart_last">
+                    <div class="product_cart_img">
+                        <img src="../images/古着1.webp" alt="">
+                    </div>
+                    <div class="product_cart_detail">
+                        <div class="product_name">
+                            <p>Reebok/"LONDON WEWBLEY STADIUM 28.OCT.07"Foodie</p>
+                        </div>
+                        <h2 class="product_price">
+                            3,750JPY
+                        </h2>
+                    </div>
+                </div>
+                <div class="total_price">
+                    <h1>Subtotal</h1>
+                    <h1>14,850 JPY</h1>
+                </div>
+                <p>Tax included and shipping calculated at checkout.</p>
+                <div class="row_right">
+                    <a href="">Check out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once __DIR__ . '/_footer.html' ?>
+</body>
+</html>
